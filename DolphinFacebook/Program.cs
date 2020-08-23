e0dd4402b6c1b1d1b8adf5e3f8be7d7cdd2da1e9
thelittlecitizen16 <thelittlecitizen16@gmail.com>
2020-08-23 19:42:47 +0300

    Add spaces

--- DolphinFacebook/Program.cs
+++ DolphinFacebook/Program.cs
@@ -10,14 +10,18 @@ namespace DolphinFacebook
             DisplayB displyB = new DisplayB();
             DisplayC displyC = new DisplayC();
             FacebookClientFactory facebookClientFactory = new FacebookClientFactory();
+
             var facebookClientA = facebookClientFactory.CreateClient(displyA);
             var facebookClientB = facebookClientFactory.CreateClient(displyB);
             var facebookClientC = facebookClientFactory.CreateClient(displyC);
+
             facebookClientB.Subscribe(facebookClientA);
             facebookClientC.Subscribe(facebookClientA);
             facebookClientA.WriteNewWallPost("Post");
+
             facebookClientB.Unsubscribe(facebookClientA);
             facebookClientA.WriteNewWallPost("Post");
+
             facebookClientC.Unsubscribe(facebookClientA);
         }
     }
